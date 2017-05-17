f0ed7739f4f016706b299a9cbc4aba3c66053aab
kleina3 <kleina3@carleton.edu>
2017-05-16 22:48:49 -0500

    added generation, getter functions for locale

---
+++ Generation.java
@@ -0,0 +1,9 @@
+import java.util.random;
+
+public class Generation{
+    private Random random;
+
+    public Generation() {
+        this.random = new Random();
+    }
+}
--- Locale.java
+++ Locale.java
@@ -6,6 +6,15 @@ public class Locale{
     public Locale(int predPop, int preyPop){
         this.basePrey = preyPop;
         this.predList = new ArrayList<Predator>(predPop);
-        
     }
+    
+    public List<Predator> getPredList(){
+        return this.predList;
+    }
+    
+    public int getBasePrey(){
+        return this.basePrey;
+    }
+    
+    
 }
