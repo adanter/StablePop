1eb8a25de7171bd6377ea9ad6fe0789f94bed848
adanterr <adanterr@bitbucket.com>
2017-05-16 22:48:49 -0500

    Made a little work in a few places, but IntelliJ's kind of breaking, so I'm pushing what I have so that I can restart with a working project

--- Locale.java
+++ Locale.java
@@ -1,11 +1,32 @@
 //keeps track of contents of a closed environment
+/*
+    Thinking in text...
+    Our algorithm for running a locale is as follows:
+    Initialize the predator and prey populations.  All preds have the same starting kill rate.
+    For each run:
+      For each generation:
+        For each pred:
+          Use pred's kill rate to determine how much it eats.
+          Record this value with the pred.
+          Decrement the prey population by this amount.
+        Group the preds into random breeding pairs.
+        Find the number of children for each breeding pair using the fitness/reproduction function from Ito et al.
+        Use crossover to create children for each breeding pair, then apply mutation to the offspring.
+        Shuffle the list of offspring.
+        Multiply the prey population by its growth constant.
+ */
+
+
 public class Locale{
     private List<Predator> predList;
     private int basePrey;
-    
+
     public Locale(int predPop, int preyPop){
         this.basePrey = preyPop;
         this.predList = new ArrayList<Predator>(predPop);
-        
+        for (int i = 0; i < predPop; i++) {
+            Predator pred = new Predator(.01);
+
+        }
     }
 }
--- Predator.java
+++ Predator.java
@@ -1,12 +1,12 @@
 public class Predator{
     //how good the predator is at acquiring food
-    private int killRate;
-    
-    public Predator(int initRate){
-        killRate = initRate;
+    private float killRate;
+
+    public Predator(float initRate){
+        this.killRate = initRate;
     }
     
-    public int getKillRate(){
+    public float getKillRate(){
         return killRate;
     }
 }
