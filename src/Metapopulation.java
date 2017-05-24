4923c4b56eb643f290f886a42e63d9e595c80734
kleina3 <kleina3@carleton.edu>
2017-05-23 22:58:00 -0500

    made the Metapopulation class

---
+++ src/Metapopulation.java
@@ -0,0 +1,19 @@
+//whoah... so meta...
+public class Metapopulation {
+    private List popArray;
+    //the total number of populations is actually numPops squared
+    public Metapopulation(int numPops, int predPop, int preyPop, double predMortality, double predKillRate) {
+        this.popArray = new ArrayList<ArrayList<Locale>>(numPops);
+        for (int i = 0; i < numPops; i++){
+            this.popArray[i] = new ArrayList<Locale>(numPops);
+            for (int j = 0; j < numPops; j++){
+                this.popArray[i][j] = new Locale(predPop, preyPop, 
+                                         predMortality, predKillRate)
+            }
+        }
+    }
+    
+    public Locale getLocale(int x, int y) {
+        return this.popArray[x][y];
+    }
+}
