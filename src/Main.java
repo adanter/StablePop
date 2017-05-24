dc1019ac8da632b94dc8e9ce8c0c515f363b02ad
kleina3 <kleina3@carleton.edu>
2017-05-24 11:49:50 -0500

    began debugging Metapopulation

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,8 @@ public class Main{
 
         Metapopulation meta = new Metapopulation(1, 10, 600, .70, .005);
         //Locale locale = new Locale(10, 600, .70);
-        Generation generation = new Generation(1.6);
-        for (int i = 0; i < 10000; i++) {
-            generation.runGeneration(meta.getLocaleAt(0, 0));
-        
-            File output = new File("log");
+        Generation generation = new Generation(2, .01);
+        File output = new File("log");
         try {
             PrintWriter pw = new PrintWriter(output);
         } catch (FileNotFoundException oops) {
@@ -25,7 +22,7 @@ public class Main{
         for (int i = 0; i < 1000; i++) {
             sb.append(i + ",");
             sb.append("");
-            generation.runGeneration(locale);
+            generation.runGeneration(meta.getLocaleAt(0, 0));
             System.out.println("--Round " + (i+1) + "--");
             System.out.println("Pred pop = " + meta.getNumPredsAt(0, 0));
             System.out.println("Prey pop = " + meta.getNumPreyAt(0, 0));
--- src/Metapopulation.java
+++ src/Metapopulation.java
@@ -1,29 +1,32 @@
 import java.util.List;
+import java.util.ArrayList;
 
 //whoah... so meta...
 public class Metapopulation {
     private List popArray;
     //the total number of populations is actually numPops squared
     public Metapopulation(int numPops, int predPop, int preyPop, double predMortality, double predKillRate) {
+        
         this.popArray = new ArrayList<ArrayList<Locale>>(numPops);
+        
         for (int i = 0; i < numPops; i++){
-            this.popArray[i] = new ArrayList<Locale>(numPops);
+            this.popArray.add(i, new ArrayList<Locale>(numPops));
             for (int j = 0; j < numPops; j++){
-                this.popArray[i][j] = new Locale(predPop, preyPop, 
-                                         predMortality, predKillRate);
+                this.popArray.get(i).add(j, new Locale(predPop, preyPop, 
+                                         predMortality, predKillRate));
             }
         }
     }
     
     public Locale getLocaleAt(int x, int y) {
-        return this.popArray[x][y];
+        return this.popArray.get(x).get(y);
     }
     
     public int getNumPreyAt(int x, int y) {
-        return this.popArray[x][y].getBasePrey();
+        return getLocaleAt(x, y).getBasePrey();
     }
     
     public int getNumPredsAt(int x, int y) {
-        return this.popArray[x][y].getPredList().size();
+        return getLocaleAt(x, y).getPredList().size();
     }
 }
