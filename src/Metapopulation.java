ae8a69939302c0dc52eb5531b50c59f2175d1edb
adanterr <adanterr@bitbucket.com>
2017-05-24 12:00:05 -0500

    Debugged basic metapopulation

--- src/Metapopulation.java
+++ src/Metapopulation.java
@@ -1,31 +1,34 @@
 import java.util.List;
 import java.util.ArrayList;
 
+
 //whoah... so meta...
 public class Metapopulation {
-    private List popArray;
+    private List<List<Locale>> popArray;
     //the total number of populations is actually numPops squared
     public Metapopulation(int numPops, int predPop, int preyPop, double predMortality, double predKillRate) {
-        
-        this.popArray = new ArrayList<ArrayList<Locale>>(numPops);
-        
+
+        this.popArray = new ArrayList<>(numPops);
+
         for (int i = 0; i < numPops; i++){
-            this.popArray.add(i, new ArrayList<Locale>(numPops));
+            //this.popArray.add(i, new ArrayList<Locale>(numPops));
+            ArrayList<Locale> localeList = new ArrayList<>(numPops);
             for (int j = 0; j < numPops; j++){
-                this.popArray.get(i).add(j, new Locale(predPop, preyPop, 
+                localeList.add(j, new Locale(predPop, preyPop,
                                          predMortality, predKillRate));
             }
+            this.popArray.add(i, localeList);
         }
     }
     
     public Locale getLocaleAt(int x, int y) {
         return this.popArray.get(x).get(y);
     }
-    
+
     public int getNumPreyAt(int x, int y) {
         return getLocaleAt(x, y).getBasePrey();
     }
-    
+
     public int getNumPredsAt(int x, int y) {
         return getLocaleAt(x, y).getPredList().size();
     }
