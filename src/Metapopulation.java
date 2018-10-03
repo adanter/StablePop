5d4d5559a2a35ea53fb97e534afa9ee5b12ae1a8
adanterr <adanterr@gmail.com>
2018-10-03 17:07:17 -0500

    Migration now takes place between adjacent locales, not random pairs of locales

--- src/Metapopulation.java
+++ src/Metapopulation.java
@@ -1,7 +1,9 @@
 /**
- * A metapopulation is an array of individual locations, each with their own predator and prey populations.  Functions
- * within Metapopulation allow individuals to move from one population to neighboring populations, allowing for some
- * genetic mixing but mostly allowing decimated locations to be repopulated by new colonists.
+ * A metapopulation is an array of individual locales, each with their own
+ * predator and prey populations.  Functions within Metapopulation allow 
+ * individuals to move from one population to neighboring populations.  This 
+ * allows for some genetic mixing, but more importantly allows decimated
+ * locales to be repopulated.
  */
 
 import java.util.List;
@@ -20,11 +22,14 @@ public class Metapopulation {
      * @param yDimension Height of the metapopulation
      * @param predPop Starting number of predators for each location
      * @param preyPop Starting number of prey for each location
-     * @param lowerKillRate Lower bound for starting kill rate for predators.  Technically, the order in which the two
-     *                      bounds are passed does not matter, since the math will produce the same result either way.
+     * @param lowerKillRate Lower bound for starting kill rate for predators
      * @param upperKillRate Upper bound for starting kill rate for predators
+     * @param random Random for use in math
      */
-    public Metapopulation(int xDimension, int yDimension, int predPop, int preyPop, double lowerKillRate, double upperKillRate, Random random) {
+    public Metapopulation(int xDimension, int yDimension, int predPop, 
+        int preyPop, double lowerKillRate, double upperKillRate, 
+        Random random) {
+
         this.xDimension = xDimension;
         this.yDimension = yDimension;
         this.popArray = new ArrayList<>(xDimension);
@@ -42,37 +47,67 @@ public class Metapopulation {
     }
 
     /**
-     * Chooses two locations at random (possibly the same location) and moves a predator from one to the other, if the
-     * former has any predators
+     * Gives each locale a chance to allow emigration of its predators and prey
+     * to an adjacent locale in the grid, then moves predators and prey
+     * accordingly.
+     *
+     * "Adjacent" in this case means straight lines only, not diagonals.  Also,
+     * the grid "wraps", so a locale on the far left side counts as adjacent to
+     * its counterpart on the far right side, and so on.
+     *
      * @param migrationRate Chance that migration will occur any given locale
-     * @param migrationChance Chance that a given predator will migrate
-     * @param preyMigrationChance Proportion of prey that will migrate when their locale is open
+     * @param migrationChance Chance that any given predator will migrate when
+     *                        its locale allows
+     * @param preyMigrationChance Fraction of prey that will migrate when the
+     *                            locale allows
      */
     public void migrate(double migrationRate, double migrationChance, double preyMigrationChance){
-        Locale srcLoc;
-        Locale newLoc;
+        Locale sourceLoc;
+        Locale destLoc;
+        // Coordinates for destination locale
         int x2;
         int y2;
         for (int x = 0; x < xDimension; x++){
             for (int y = 0; y < yDimension; y++){
                 if (random.nextFloat() < migrationRate){
-                    srcLoc = getLocaleAt(x, y);
-
-                    x2 = Math.floorMod((x + (random.nextInt(xDimension) - 1)) , xDimension);
-                    y2 = Math.floorMod((y + (random.nextInt(yDimension) - 1)) , yDimension);
-                    newLoc = getLocaleAt(x2, y2);
+                    sourceLoc = getLocaleAt(x, y);
+                    int direction = random.nextInt(4);
+                    switch (direction) {
+                        // East
+                        case 0:
+                            x2 = Math.floorMod(x+1, xDimension);
+                            y2 = y;
+                            break;
+                        // North
+                        case 1:
+                            x2 = x;
+                            y2 = Math.floorMod(y+1, yDimension);
+                            break;
+                        // West
+                        case 2:
+                            x2 = Math.floorMod(x-1, xDimension);
+                            y2 = y;
+                            break;
+                        // South
+                        default:
+                            x2 = x;
+                            y2 = Math.floorMod(y-1, yDimension);
+                            break;
+                    }
+                    destLoc = getLocaleAt(x2, y2);
                     
-                    int max = srcLoc.getNumPreds();
-                    for (int i = 0; i < max; i++){
+                    int max = sourceLoc.getNumPreds();
+                    for (int predIndex = 0; predIndex < max; predIndex++){
                         if (random.nextFloat() < migrationChance){
-                            newLoc.addPred(srcLoc.popPred(i));
+                            destLoc.addPred(sourceLoc.popPred(predIndex));
                             max--;
                         }
                     }
                     if (preyMigrationChance > 0) {
-                        int preyTransfer = (int)(srcLoc.getNumPrey() * preyMigrationChance);
-                        srcLoc.reduceBasePrey(preyTransfer);
-                        newLoc.setNumPrey(newLoc.getNumPrey() + preyTransfer);
+                        int preyTransfer = (int)(sourceLoc.getNumPrey() 
+                            * preyMigrationChance);
+                        sourceLoc.reduceBasePrey(preyTransfer);
+                        destLoc.setNumPrey(destLoc.getNumPrey() + preyTransfer);
                     }
                 }
             }
@@ -131,9 +166,9 @@ public class Metapopulation {
      */
     public int getTotalPrey() {
         int totalPrey = 0;
-        for (int i = 0; i < xDimension; i++) {
-            for (int j = 0; i < yDimension; i++) {
-                totalPrey += getLocaleAt(i,j).getNumPrey();
+        for (int x = 0; x < xDimension; x++) {
+            for (int y = 0; y < yDimension; y++) {
+                totalPrey += getLocaleAt(x,y).getNumPrey();
             }
         }
         return totalPrey;
@@ -145,9 +180,9 @@ public class Metapopulation {
      */
     public int getTotalPreds() {
         int totalPreds = 0;
-        for (int i = 0; i < xDimension; i++) {
-            for (int j = 0; i < yDimension; i++) {
-                totalPreds += getLocaleAt(i,j).getNumPreds();
+        for (int x = 0; x < xDimension; x++) {
+            for (int y = 0; y < yDimension; y++) {
+                totalPreds += getLocaleAt(x,y).getNumPreds();
             }
         }
         return totalPreds;
